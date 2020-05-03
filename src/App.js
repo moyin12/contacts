5c66fd1ab5592c1d80133138882d5732563ccd3f
Moyinoluwa Boluwatife Ariyo <moyin.ariyo@gmail.com>
2020-05-03 14:00:32 +0100

    Creating a createcontact component to teach react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
 
 class App extends Component {
   state ={
-    contacts: []
+    contacts: [],
+    screen: 'create',
   }
   componentDidMount(){
     ContactsAPI.getAll()
@@ -21,13 +23,15 @@ class App extends Component {
           return c.id !== contact.id
         })
       }))
+      ContactsAPI.remove(contact)
   }
   render() {
     return (
       <div>
-        <ListContacts 
+        {this.state.screen === 'list' && (<ListContacts 
         onDeleteContact = {this.removeContact}
-        contacts = {this.state.contacts}/>
+        contacts = {this.state.contacts}/>)}
+        {this.state.screen === 'create' && <CreateContact />}
       </div>
     );
   }
